ddd2d74765eb0588ecc54fa5c077e6439efa10f4
itzjustalan <itzjustalan@gmail.com>
2023-05-30 19:35:20 +0530

    dynamiggyyyy

---
+++ js/data.js
@@ -0,0 +1,7 @@
+import Queue from './ds/queue.js'
+import Stack from './ds/stack.js'
+
+export const data_structures = {
+    queue: { v: new Queue(), file: 'queue.js' },
+    stack: { v: new Stack(), file: 'stack.js' },
+}
